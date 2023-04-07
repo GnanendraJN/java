790d96a228a69ff679b8f79c3070e4d91a7b564d
Gnanendra JN <gnanendra.jn@gmail.com>
2023-04-08 00:09:43 +0530

    List of Java 8 features

---
+++ java/src/main/java/org/learn/java8/Java8Features.java
@@ -0,0 +1,17 @@
+package org.learn.java8;
+
+public class Java8Features {
+    public static void main(String[] args) {
+        String features = "1. Lambda Expressions \n " +
+                "2. Functional Interface \n " +
+                "3. Streams \n " +
+                "4. New Date and Time APIs  \n " +
+                "5. Method Reference \n " +
+                "6. Default Methods \n " +
+                "7. Static Methods in Interfaces \n " +
+                "8. Optional \n " +
+                "9. IO Enhancements \n ";
+
+        System.out.println("Java 8 features are \n" + features);
+    }
+}
